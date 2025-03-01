24b53d33d545523c76724dc1ddc9f22a5b758838
VencelV <152347767+VencelV@users.noreply.github.com>
2025-03-01 14:09:20 -0600

    Update servo_test1.py

--- servo_test1.py
+++ servo_test1.py
@@ -8,7 +8,7 @@ pwm = PWM(0, 0)  # Correcting the PWM chip input
 pwm.enable()
 
 # Set frequency to 50Hz (20ms period for servos)
-pwm.frequency = 50
+pwm.frequency = 200
 
 # Function to set servo angle
 def set_servo_angle(angle):
